53ffaa4d148ce227d9ba9b1d2d1ae520ff013f36
shlrkb <31007911+shlrkb@users.noreply.github.com>
2023-08-24 15:38:18 +0200

    Update rsync-hetzner.sh
    adding public_html/pub/media/mf_webp to exclude

--- rsync-hetzner.sh
+++ rsync-hetzner.sh
@@ -19,7 +19,7 @@ RSYNC_BIN=/usr/bin/rsync
 RSYNC_OPTS="--progress -azh  --delete --ignore-errors --inplace --delete-excluded --force -4"
 SSH_OPTS="-o ConnectTimeout=5"
 
-ROPTS="--exclude public_html/var --exclude public_html/media/catalog/product/cache --exclude public_html/pub/media/catalog/product/cache"
+ROPTS="--exclude public_html/var --exclude public_html/pub/media/mf_webp --exclude public_html/media/catalog/product/cache --exclude public_html/pub/media/catalog/product/cache"
 
 #checking if DB directory exists. else create
 if [ ! -d "$MYSQLDIR" ]; then
