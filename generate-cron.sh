8e84da99adb44def7345e8766b5affe16bfc15e1
shlrkb <31007911+shlrkb@users.noreply.github.com>
2023-08-28 09:23:06 +0200

    Update generate-cron.sh

--- generate-cron.sh
+++ generate-cron.sh
@@ -3,4 +3,4 @@
 HOUR=$(( $RANDOM % 6 + 3 ))
 MINUTE=$(( $RANDOM % 59 + 1 ))
 
-echo "$MINUTE $HOUR * * * /usr/bin/bash $HOME/remote-backup/rsync-hetzner.sh &>/dev/null"
+echo "$MINUTE $HOUR * * * /bin/bash $HOME/remote-backup/rsync-hetzner.sh &>/dev/null"
